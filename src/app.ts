69da2de6d24cd5608919e1869003e4ff4b3175b4
Cezar_Bacanieski <bacanieskidev@gmail.com>
2024-09-23 19:14:45 -0300

    feat: adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express, { Application } from 'express';
 import { AppDataSource } from './config/database';
 import router from './routes';
 
 const app: Application = express();
+
+const corsOptions = {
+  origin: ['*'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Length', 'X-Kuma-Revision'],
+  credentials: true, //
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 router(app);
