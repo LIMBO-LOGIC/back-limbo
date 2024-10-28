758457f8a12a69dddb1ea4b9896372c3886a0b93
Cezar_Bacanieski <bacanieskidev@gmail.com>
2024-10-28 12:43:48 -0300

    fix: arrumando bug de rotas do user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,16 +4,19 @@ import { UserController } from '../controllers/UserController';
 const router = Router();
 const userController = new UserController();
 
+// Rotas de criação e autenticação
 router.post('/user/register', userController.register.bind(userController));
 router.post('/user/login', userController.login.bind(userController));
+
+// Rotas de busca específicas e genéricas
+router.get('/user/:id', userController.getUser.bind(userController));
 router.get(
   '/user/points',
   userController.getUsersByPoints.bind(userController)
 );
-
-router.get('/user/:id', userController.getUser.bind(userController));
-
 router.get('/users', userController.getAllUser.bind(userController));
+
+// Rotas de atualização de dados do usuário
 router.put('/user/:id', userController.updateUser.bind(userController));
 router.put(
   '/user/:id/points',
@@ -24,6 +27,7 @@ router.put(
   userController.changePassword.bind(userController)
 );
 
+// Rotas de exclusão e desativação
 router.delete('/user/:id', userController.deactivateUser.bind(userController));
 router.delete(
   '/user/delete/:id',
